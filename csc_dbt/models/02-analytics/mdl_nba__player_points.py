2875927ae3c31c5681ab84456137a463c5eaae0e
Ian <47256454+IanRFerguson@users.noreply.github.com>
2025-09-16 21:52:08 -0400

    clean run

--- csc_dbt/models/02-analytics/mdl_nba__player_points.py
+++ csc_dbt/models/02-analytics/mdl_nba__player_points.py
@@ -1,5 +1,4 @@
 from sklearn.linear_model import LinearRegression
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
 MODEL_FEATURES = [
@@ -15,16 +14,24 @@ MODEL_FEATURES = [
 
 # NOTE - dbt and session are required parameters for dbt models
 def model(dbt, session):
-    # Load the data
-    df = dbt.ref("stg_nba__player_data").toPandas()
+    # Load the required columns to train our model
+    target_columns = MODEL_FEATURES + ["points", "player_id"]
+    df = dbt.ref("stg_nba__player_data").select(*target_columns).toPandas()
+
+    # A little naive but we'll impute 0's for missing data
+    df.fillna(0, inplace=True)
 
     # Select features and target variable
     features = df[MODEL_FEATURES]
-    target = df["points"]
+
+    for feature in MODEL_FEATURES:
+        print(f"{feature}: {features[feature].isna().sum()} missing values")
+
+    target = df["points"].fillna(0)
 
     # Split the data into training and testing sets
     X_train, _, y_train, _ = train_test_split(
-        features, target, train_size=0.5, random_state=101
+        features, target, train_size=0.3, random_state=101
     )
 
     # Create and train the model
